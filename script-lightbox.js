3252b4248723a298fc060e86b207cc34946aee04
Rockwell15 <rockwell15@hotmail.com>
2016-09-01 17:24:25 -0400

    Create script-lightbox.js

---
+++ script-lightbox.js
@@ -0,0 +1,33 @@
+
+//---- Lightbox
+jQuery('body').append('<div id="social-feeder-lightbox-overlay"></div>');
+jQuery('body').append('<div id="social-feeder-lightbox-container"></div>');
+
+jQuery(document).on('click', '.social-feeder-lightbox', function() {
+	jQuery('#social-feeder-lightbox-overlay').fadeIn(350);
+
+	var img = new Image();
+	img.onload = function () {
+		var imgRatio = img.width / img.height;
+		var windowRatio = window.innerWidth / window.window.innerHeight;
+
+		img.style.maxWidth = img.width + 'px';
+		img.style.maxHeight = img.height + 'px';
+		if ( imgRatio > windowRatio ) {
+			img.width  = .8 * window.innerWidth;
+			img.style.height = 'auto';
+		} else {
+			img.style.width  = 'auto';
+			img.height = .8 * window.window.innerHeight;
+		}
+
+		jQuery('#social-feeder-lightbox-container').append( img ).delay(50).fadeIn(350);
+	}
+	img.src = this.src;
+});
+
+jQuery(document).on('click', '#social-feeder-lightbox-overlay', function() {
+	jQuery('#social-feeder-lightbox-container, #social-feeder-lightbox-overlay').fadeOut(350, function() {
+		jQuery('#social-feeder-lightbox-container').html('');
+	});
+});
